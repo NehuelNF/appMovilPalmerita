ebc5861c9d9edea157351613696947c1f80b4cb6
Nelsoninho-gg <ne.alfaro@duocuc.cl>
2024-12-02 22:20:34 -0300

    Añadir configuración inicial de Android y archivos de recursos para la aplicación

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyB6iS9GTE4KGFX0FlORWexHwCDPV6t2MRA",
+    authDomain: "palmeritapp.firebaseapp.com",
+    databaseURL: "https://palmeritapp-default-rtdb.firebaseio.com",
+    projectId: "palmeritapp",
+    storageBucket: "palmeritapp.appspot.com",
+    messagingSenderId: "280120879583",
+    appId: "1:280120879583:web:cace7d50616179e8fdfc6a",
+    measurementId: "G-QETJHWTL2J"
+  }
 };
